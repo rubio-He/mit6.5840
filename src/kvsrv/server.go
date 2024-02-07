70645c37a2ee963515feed83023fdfa8b3e81d9a
Ruibo He <hobbes.ruibohe@gmail.com>
2024-02-07 13:04:07 -0800

    Rename variables and free server's mem after confirming op is done.

--- src/kvsrv/server.go
+++ src/kvsrv/server.go
@@ -20,10 +20,10 @@ type Job struct {
 }
 
 type KVServer struct {
-	dedup  map[int64]int64
-	kv     map[string]string
-	kv_old map[Job]string
-	mu     sync.Mutex
+	pendingOp   map[int64]int64 // Mapping from client id to op id
+	kv          map[string]string
+	pendingOldV map[Job]string // Pending appending op's value
+	mu          sync.Mutex
 }
 
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
@@ -42,12 +42,12 @@ func (kv *KVServer) Put(args *PutAppendArgs, reply *PutAppendReply) {
 	kv.mu.Lock()
 	defer kv.mu.Unlock()
 	DPrintf("Receive client put request key %s value %s from %d at uuid %d", args.Key, args.Value, args.ClientId, args.Uuid)
-	if uuid, ok := kv.dedup[args.ClientId]; ok {
+	if uuid, ok := kv.pendingOp[args.ClientId]; ok {
 		if uuid == args.Uuid {
 			return
 		}
 	}
-	kv.dedup[args.ClientId] = args.Uuid
+	kv.pendingOp[args.ClientId] = args.Uuid
 	kv.kv[args.Key] = args.Value
 	DPrintf("After put kv '%s'", kv.kv)
 }
@@ -56,18 +56,23 @@ func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
 	kv.mu.Lock()
 	defer kv.mu.Unlock()
 	DPrintf("Receive client append request key %s value %s from %d at uuid %d", args.Key, args.Value, args.ClientId, args.Uuid)
-	if uuid, ok := kv.dedup[args.ClientId]; ok {
+	if uuid, ok := kv.pendingOp[args.ClientId]; ok {
 		if uuid == args.Uuid {
-			reply.Value = kv.kv_old[Job{args.Uuid, args.ClientId}]
+			reply.Value = kv.pendingOldV[Job{args.Uuid, args.ClientId}]
 			return
 		}
 	}
 
-	kv.dedup[args.ClientId] = args.Uuid
+	// Remove pending op's value.
+	oldId := kv.pendingOp[args.ClientId]
+	delete(kv.pendingOldV, Job{oldId, args.ClientId})
+
+	// Update pending op's id
+	kv.pendingOp[args.ClientId] = args.Uuid
 
 	if val, ok := kv.kv[args.Key]; ok {
 		kv.kv[args.Key] = val + args.Value
-		kv.kv_old[Job{args.Uuid, args.ClientId}] = val
+		kv.pendingOldV[Job{args.Uuid, args.ClientId}] = val
 		reply.Value = val
 	} else {
 		kv.kv[args.Key] = args.Value
@@ -79,8 +84,8 @@ func StartKVServer() *KVServer {
 	kv := new(KVServer)
 
 	kv.kv = make(map[string]string)
-	kv.dedup = make(map[int64]int64)
-	kv.kv_old = make(map[Job]string)
+	kv.pendingOp = make(map[int64]int64)
+	kv.pendingOldV = make(map[Job]string)
 
 	return kv
 }
